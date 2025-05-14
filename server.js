7c74e0e378953a274c2943fd4900c42a160c6c70
Oleksii Ozeranets <bomok122@gmail.com>
2025-05-14 01:23:11 -0400

    add button after click send html content

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 const express = require("express");
 const readline = require("readline");
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
-let userInput = "";
+app.use(cors({
+  origin: "*", // Allow requests from any origin
+  credentials: true // If you’re sending cookies or authorization headers
+}));
 
-async function askUserInput() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  userInput = await new Promise((resolve) => {
-    rl.question("Please enter a value: ", (answer) => {
-      rl.close();
-      resolve(answer);
-    });
-  });
-}
-
-app.get("/api/input", (req, res) => {
-  res.json({ value: userInput });
+app.use(express.text({ type: 'text/html' }));
+app.post("/api/generate-cv", (req, res) => {
+  console.log(req.get('accept'))
+  console.log(req.body)
+  res.json({ value: 'ok' });
 });
 
-askUserInput().then(() => {
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
     console.log("GET /api/input to see the value you entered.");
   });
-});
